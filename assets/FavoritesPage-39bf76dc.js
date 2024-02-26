5fa0c07d23ff42ddd6b6258c8e5325aa3df0688b
TanyaMosk <123247337+TanyaMosk@users.noreply.github.com>
2024-02-26 17:49:06 +0000

    Deploying to gh-pages from @ TanyaMosk/psychologists-services@25ce09a537921855b82871b36ae4fc91c77a59b5 🚀

---
+++ assets/FavoritesPage-39bf76dc.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-6ab5f72c.js";const s=()=>e.jsx("div",{children:e.jsx("h1",{children:"Favorites Page"})});export{s as default};
---
+++ assets/PsychologistsPage-310dc22e.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-6ab5f72c.js";const t=()=>s.jsx("div",{children:s.jsx("h1",{children:"Psychologists Page"})});export{t as default};
